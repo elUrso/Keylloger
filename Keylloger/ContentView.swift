5785c4ef14ccfc85ae3fdcc50a7c483744a4b829
Vitor Silva <sirmochi45@gmail.com>
2020-03-11 20:29:54 -0300

    Initial Commit

---
+++ Keylloger/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Keylloger
+//
+//  Created by Vitor Silva on 11/03/20.
+//  Copyright © 2020 Vitor Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
